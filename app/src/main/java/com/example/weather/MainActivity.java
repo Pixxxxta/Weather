fe261015218b124d51eef78108ee5d4078220e36
Pixxxxta <krapachev@gmail.com>
2022-05-18 20:51:59 +0300

    Weather

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weather;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -16,11 +17,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -31,48 +36,126 @@ import java.net.URL;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+
     private LocationManager locationManager;
     StringBuilder sbGPS = new StringBuilder();
-
-
-
+    FileInputStream fin = null;
+    String[][] myArray = {{"50","50"}, {"54", "55"}, {"61", "70"}, {"55", "60"},  {"70", "60"}, {"62", "60"}};
     private Button button;
     private TextView textView;
     private TextView country;
     private TextView humidity;
     private TextView pressure;
     private ImageView imageView;
+    private Button right;
+    private Button left;
+    private int pos = -1;
+    private String lat;
+    private String lon;
+    private boolean check = false;
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String url = "";
+        try {
+            Bundle arguments = getIntent().getExtras();
+            String name = arguments.get("hello").toString();
+            System.out.println(name + "АААААААААААААААААААААААААААААААААААААААААААААААА");
+            url = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+            check = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         button = findViewById(R.id.button);
+        right = findViewById(R.id.button3);
+        left = findViewById(R.id.button4);
+
         textView = findViewById(R.id.textView);
         country = findViewById(R.id.textView2);
         humidity = findViewById(R.id.textView3);
         pressure = findViewById(R.id.textView4);
         imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.a01d);
+        imageView.setImageResource(R.drawable.start);
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, locationListener);
 
         String messageText = showLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
         System.out.println(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude());
         System.out.println(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude());
-        String url = "https://api.openweathermap.org/data/2.5/weather?lat="+locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude()+"&lon=" + locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude() + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+        if (check == false){
+            url = "https://api.openweathermap.org/data/2.5/weather?lat="+locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude()+"&lon=" + locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude() + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+        }
+
+        String url1 = "https://api.openweathermap.org/data/2.5/weather?lat=50&lon=-132&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
         new GetUrlData().execute(url);
         // кнопка бесполезная
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String city = "Moscow";
                 String url = "https://api.openweathermap.org/data/2.5/weather?lat="+locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude()+"&lon=" + locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude() + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+                String url1 = "https://api.openweathermap.org/data/2.5/weather?lat=50&lon=-132&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
                 new GetUrlData().execute(url);
 
 
             }
         });
+
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url1 = "";
+                pos = pos - 1;
+                System.out.println(pos);
+
+                if (pos < 0){
+                    pos = -1;
+                    url1 = "https://api.openweathermap.org/data/2.5/weather?lat="+locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude()+"&lon=" + locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude() + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+
+                    System.out.println(url1);
+                    new GetUrlData().execute(url1);
+                }
+                else{
+                    url1 = "https://api.openweathermap.org/data/2.5/weather?lat="+myArray[pos][0]+"&lon=" + myArray[pos][1] + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+                    System.out.println(url1);
+                    new GetUrlData().execute(url1);
+
+                }
+
+
+
+            }
+        });
+
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url1 = "";
+                pos = pos + 1;
+                System.out.println(pos);
+                if (pos >= myArray.length){
+                    pos = myArray.length-1;
+                    url1 = "https://api.openweathermap.org/data/2.5/weather?lat="+myArray[pos][0]+"&lon=" + myArray[pos][1] + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+                    System.out.println(url1);
+
+                }
+                else{
+                    url1 = "https://api.openweathermap.org/data/2.5/weather?lat="+myArray[pos][0]+"&lon=" + myArray[pos][1] + "&appid=508dabe2e8078280e8b3e4198e0c0068&lang=ru&units=metric";
+                    System.out.println(url1);
+                    new GetUrlData().execute(url1);
+                }
+
+
+
+            }
+        });
+
 
 
 
@@ -157,6 +240,68 @@ public class MainActivity extends AppCompatActivity {
                 country.setText(obj.getString("name"));
                 humidity.setText("Влажность воздуха " + obj.getJSONObject("main").getDouble("humidity"));
                 pressure.setText("Давление атм: " + obj.getJSONObject("main").getDouble("pressure"));
+                JSONArray obj1 = obj.getJSONArray("weather");
+                String obj2 = obj1.getJSONObject(0).getString("icon");
+                if (obj2.equals("01d")){
+                    imageView.setImageResource(R.drawable.a01d);
+                }
+                if (obj2.equals("01n")){
+                    imageView.setImageResource(R.drawable.a01n);
+                }
+                if (obj2.equals("02d")){
+                    imageView.setImageResource(R.drawable.a02d);
+                }
+                if (obj2.equals("02n")){
+                    imageView.setImageResource(R.drawable.a02n);
+                }
+                if (obj2.equals("03d")){
+                    imageView.setImageResource(R.drawable.a03d);
+                }
+                if (obj2.equals("03n")){
+                    imageView.setImageResource(R.drawable.a03n);
+                }
+                if (obj2.equals("04d")){
+                    imageView.setImageResource(R.drawable.a04d);
+                }
+                if (obj2.equals("04n")){
+                    imageView.setImageResource(R.drawable.a04n);
+                }
+                if (obj2.equals("09d")){
+                    imageView.setImageResource(R.drawable.a09d);
+                }
+                if (obj2.equals("09n")){
+                    imageView.setImageResource(R.drawable.a09n);
+                }
+                if (obj2.equals("10d")){
+                    imageView.setImageResource(R.drawable.a10d);
+                }
+                if (obj2.equals("10n")){
+                    imageView.setImageResource(R.drawable.a10n);
+                }
+                if (obj2.equals("11d")){
+                    imageView.setImageResource(R.drawable.a10d);
+                }
+                if (obj2.equals("10n")){
+                    imageView.setImageResource(R.drawable.a10n);
+                }
+                if (obj2.equals("11d")){
+                    imageView.setImageResource(R.drawable.a11d);
+                }
+                if (obj2.equals("11n")){
+                    imageView.setImageResource(R.drawable.a11n);
+                }
+                if (obj2.equals("13d")){
+                    imageView.setImageResource(R.drawable.a13d);
+                }
+                if (obj2.equals("13n")){
+                    imageView.setImageResource(R.drawable.a13n);
+                }
+                if (obj2.equals("50d")){
+                    imageView.setImageResource(R.drawable.a50d);
+                }
+                if (obj2.equals("50n")){
+                    imageView.setImageResource(R.drawable.a50n);
+                }
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -170,4 +315,11 @@ public class MainActivity extends AppCompatActivity {
                 location.getLatitude(), location.getLongitude(), new Date(
                         location.getTime()));
     }
+
+    public void onCountry(View view) {
+        Intent intent = new Intent(this, CountryActivity.class);
+        startActivity(intent);
+
+    }
+
 }
